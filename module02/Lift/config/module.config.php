d86465735ef9518338a7967a2b4b0371d20c9516
Peter Marko <peter.marko.ext@gmail.com>
2019-09-17 14:17:51 +0100

    Raw validator implementation demo.

--- module02/Lift/config/module.config.php
+++ module02/Lift/config/module.config.php
@@ -2,11 +2,33 @@
 namespace Lift;
 
 use Lift\Controller\IndexController;
+use Lift\Controller\UserController;
+use Zend\Filter\Callback;
+use Zend\Validator\StringLength;
 
 return [
+    'validators' => [
+        'factories' => [
+            'GreaterThan5' => function($pm){
+                return new StringLength(['min' => 5]);
+            }
+        ]
+    ],
+    'filters' => [
+        'factories' => [
+            'UppercaseFirst' => function($pm){
+                $filter = new Callback();
+                $filter->setCallback(function($val){
+                    return ucfirst($val);
+                });
+                return $filter;
+            }
+        ]
+    ],
     'controllers' => [
         'invokables' => [
-            'Lift\Controller\Index' => IndexController::class
+            'Lift\Controller\Index' => IndexController::class,
+            'Lift\Controller\User'  => UserController::class
         ],
     ],
     'router' => [
@@ -20,6 +42,42 @@ return [
                         'action'     => 'index',
                     ],
                 ],
+                'may_terminate' => true,
+                'child_routes' => [
+                    'register' => [
+                        'type' => 'Zend\Mvc\Router\Http\Literal',
+                        'options' => [
+                            'route'    => '/register',
+                            'defaults' => [
+                                'controller' => 'Lift\Controller\User',
+                                'action'     => 'register',
+                            ],
+                        ],
+                    ],
+//                    'user' => [
+////                        'type' => 'Zend\Mvc\Router\Http\Literal',
+////                        'options' => [
+////                            'route'    => '/user',
+////                            'defaults' => [
+//////                                'controller' => 'Lift\Controller\Index',
+//////                                'action'     => 'index',
+////                            ],
+////                        ],
+////                        'may_terminate' => false,
+//////                        'child_routes' => [
+//////                            'register' => [
+//////                                'type' => 'Zend\Mvc\Router\Http\Literal',
+//////                                'options' => [
+//////                                    'route'    => '/register',
+//////                                    'defaults' => [
+//////                                        //'controller' => 'Lift\Controller\Index',
+//////                                        //'action'     => 'index',
+//////                                    ],
+//////                                ],
+//////                            ]
+//////                        ]
+////                    ]
+                ]
             ],
         ],
     ],
